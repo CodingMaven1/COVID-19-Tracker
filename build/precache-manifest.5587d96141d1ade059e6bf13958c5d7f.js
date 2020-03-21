497f9c02f2efe1c1835093eaee2363a464c36f04
Naman Agarwal <namana019@outlook.com>
2020-03-21 15:16:03 +0530

    Updated package.json

---
+++ build/precache-manifest.5587d96141d1ade059e6bf13958c5d7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed101da80bd8a9cad5073568545ae2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50ed48ef5f7fdfb3101c",
+    "url": "/static/css/main.0c69f3aa.chunk.css"
+  },
+  {
+    "revision": "7201fb919e68a265336f",
+    "url": "/static/js/2.356af582.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.356af582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50ed48ef5f7fdfb3101c",
+    "url": "/static/js/main.4a2dd1be.chunk.js"
+  },
+  {
+    "revision": "2d8f72aeca456b536118",
+    "url": "/static/js/runtime-main.c2717fbf.js"
+  }
+]);
